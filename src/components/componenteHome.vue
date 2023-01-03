6903dc84b448402d1436a107049c0a345f00907b
Anibolitaa <0619971046@uma.es>
2023-01-03 13:07:56 +0100

    Cambiado el redirect

--- src/components/componenteHome.vue
+++ src/components/componenteHome.vue
@@ -27,17 +27,23 @@ export default{
     <table class="tabla-Inicio">    
         <tr v-if="token">
             <td class="text-button"><router-link to="/crearAnuncio" ><a class="boton_personalizado">Crear anuncio</a></router-link></td>
-            <td class="text-cell">Describir la creacion de un anuncio</td>
+            <td class="text-cell">¿Tiene viviendas vacias y le gustaria ganar un dinero? Entonces comienze registrando su casa en nuestra aplicación.
+                                    ¡Introduciendo pocos datos podra convertir su casa en una residencia vacacional y ganarse un dinero extra!
+            </td>
         </tr>
 
         <tr>
-            <td class="text-cell">Describir lo que hay en ver anuncios</td>
+            <td class="text-cell">¿Esta buscando donde pasar las vacaciones? Entonces mire todas las ofertas de residencias vacacionales que ofrecen nuestros usuarios.
+                                    Cuando le guste una podra reservarlá en las fechas que usted prefieras, realizando un pago seguro a traves de PayPal. ¿A qué espera?
+            </td>
             <td class="text-button"><router-link to="/listaViviendas" ><a class="boton_personalizado" href="">Ver anuncios</a></router-link></td>
         </tr>
 
         <tr>
             <td class="text-button"><router-link to="/verTiempo" ><a class="boton_personalizado" href="">Consultar tiempo</a></router-link></td>
-            <td class="text-cell">Describir lo que hace el boton de ver el tiempo</td>
+            <td class="text-cell">¿No se decide a donde ir de vacaciones? Consulte el tiempo de sus zonas vacacionales preferidas. No querrá ir a la 
+                                    playa con lluvia o a esquiar sin nieve, ¿verdad?
+            </td>
         </tr>
 
     </table>
